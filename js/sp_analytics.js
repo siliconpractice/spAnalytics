8294e347973656ee34b0d4b1e98ea118bd29200e
charlotterees <51909209+charlotterees@users.noreply.github.com>
2019-08-22 16:00:38 +0100

    Update

--- js/sp_analytics.js
+++ js/sp_analytics.js
@@ -1,5 +1,13 @@
 jQuery(document).ready(function() {
-    jQuery('body').on('click', 'a, button, .sp_track', function() {
+    jQuery('body').on('click', 'a, button, .sp_track, .frm_forms', function(e) {
+        
+        var clickcounter;
+        
+        if(e.currentTarget.classList.contains("frm_forms")) {
+            alert("you clicked on a form");
+            clickcounter++; 
+        }
+        
         jQuery.ajax({
             url: "/sp_tracking.php",
             data: {
--- sp_tracking.php
+++ sp_tracking.php
@@ -22,18 +22,29 @@ function getLogin(){
   return false;
 }
 
+$urlFilter['javascript:void(0)'] = true;
+
 list($db, $prefix) = getLogin();
 
 $returning = isset($_COOKIE['sp_longterm'])?1:0;
 
-file_put_contents("tracking.log", "Entered this routine: " . $prefix . "url: " . $_REQUEST['url'] . " text: " . $_REQUEST['text'] . " domain: " . $_SERVER['HTTP_HOST'] . " session id: " . $_COOKIE['sp_session'] . " returninguser: " . $returning, FILE_APPEND);
+//file_put_contents("tracking.log", "Entered this routine: " . $prefix . "url: " . $_REQUEST['url'] . " text: " . $_REQUEST['text'] . " domain: " . $_SERVER['HTTP_HOST'] . " session id: " . $_COOKIE['sp_session'] . " returninguser: " . $returning, FILE_APPEND);
 
 setcookie('sp_longterm', '1', time()+60*60*24*90); //<-- set perm cookie, one year
 
-$sql = $db->prepare("INSERT into {$prefix}sp_analytics(whenrecorded, url, linktext, domain, sessionid, returninguser) values(now(), ?, ?, ?, ?, ?)");
-$sql->bind_param("ssssi", $_REQUEST['url'], $_REQUEST['text'], $_SERVER['HTTP_HOST'], $_COOKIE['sp_session'], $returning);
-$sql->execute();
-$sql->close();
-echo "Ok";
-die();
+//if (!in_array($_REQUEST['url'], $urlFilter)) {
+    
+if (!isset($urlFilter[$_REQUEST['url']])) {
+    
+    file_put_contents("tracking.log", "Inside the if statement: " . $_REQUEST['url'], FILE_APPEND);
+    
+    $sql = $db->prepare("INSERT into {$prefix}sp_analytics(whenrecorded, url, linktext, domain, sessionid, returninguser) values(now(), ?, ?, ?, ?, ?)");
+    $sql->bind_param("ssssi", $_REQUEST['url'], $_REQUEST['text'], $_SERVER['HTTP_HOST'], $_COOKIE['sp_session'], $returning);
+    $sql->execute();
+    $sql->close();
+    echo "Ok";
+    die();
+} else {
+    file_put_contents("tracking.log", "Inside the else statement: " . $_REQUEST['url'], FILE_APPEND);
+}
 ?>
